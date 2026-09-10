f1364a5e53da94992a6b6310356b8e14786051e9
Italo Freitas <italofreitas.work@gmail.com>
2026-09-09 22:32:17 -0300

    Começando frontend

--- src/main/java/com/italobackend/chamadoapi/ChamadoApiApplication.java
+++ src/main/java/com/italobackend/chamadoapi/ChamadoApiApplication.java
@@ -13,6 +13,9 @@ public class ChamadoApiApplication {
         Dotenv dotenv = Dotenv.load();
         System.setProperty("JWT_SECRET", dotenv.get("JWT_SECRET"));
         System.setProperty("JWT_EXPIRATION", dotenv.get("JWT_EXPIRATION"));
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
 
         SpringApplication.run(ChamadoApiApplication.class, args);
     }
