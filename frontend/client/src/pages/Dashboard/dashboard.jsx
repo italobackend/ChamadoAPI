f588b1d3546b09c6914cddaf45bdd63f38a00407
Ítalo Freitas <italofreitas.work@gmail.com>
2026-09-23 10:00:36 -0300

    feat(dashboard, sidebar) criação dos arquivos sidebar e dashboard que aparecem após o login

---
+++ frontend/client/src/pages/Dashboard/dashboard.jsx
@@ -0,0 +1,12 @@
+import {useState} from "react";
+import {useNavigate} from "react-router-dom"
+
+import './style.css'
+import '../../index.css'
+import '../Sidebar/sidebar.jsx'
+
+function Dashboard() {
+
+}
+
+export default Dashboard
---
+++ frontend/client/src/pages/Sidebar/sidebar.jsx
@@ -0,0 +1,9 @@
+import {useState} from 'react'
+import {useNavigate} from 'react-router-dom'
+import './sidebar.css'
+import '../../index.css'
+
+function Sidebar() {
+}
+
+export default Sidebar
